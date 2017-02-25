3d5f685d970af4168811f5a6d3b551b4a8c0c5f7
KunalSinha7 <sinha33@purdue.edu>
2017-02-25 12:25:36 -0500

    Fixed speech recognition

--- MyDoctorPython.py
+++ MyDoctorPython.py
@@ -1,4 +1,11 @@
-import JSON
+"""
+This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
+as testing instructions are located at http://amzn.to/1LzFrj6
+
+For additional samples, visit the Alexa Skills Kit Getting Started guide at
+http://amzn.to/1LGWsLG
+"""
 
 from __future__ import print_function
 
@@ -43,107 +50,38 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to My Doctor. Please state up to 2 symptoms you are currently experiencing."
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please state up to 2 symptoms that you are currently experiencing."
+    speech_output = "Welcome to the MyDoc test. " \
+                    
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, none, should_end_session))
+
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "My Doctor is now closing"
+    speech_output = "Thank you for trying the MyDoc. " \
+                    "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
-
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
-def findSingleSymptom(intent, session):
-    session_attributes = {}
-    should_end_session = False
-    if 'feel' in intent['slots']:
-        symptom = intent['slots']['feel']['value']
-    if 'location' in intent['slots']:
-        location = intent['slots']['location']['value']
-    card_title = "Finding health issue"
-    speech_output = symptom + " in " + location
 
-    #TODO get health issue from database (IMO)
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-   
-
-def findDualSymptom(intent, session):
-    session_attributes = {}
-    should_end_session = False
-    if 'feelA' in intent['slot']:
-        symptom1 = intent['slots']['feelA']['value']
-    if 'feelB' in intent['slot']:
-        symptom2 = intent['slots']['feelA']['value']
-    if 'locationA' in intent['slot']:
-        symptom1 = intent['slots']['locationA']['value']
-    if 'locationB' in intent['slot']:
-        symptom1 = intent['slots']['locationB']['value']
-    card_title = "Finding health issue"
-    #TODO get health issue from database (IMO)
-    speech_output = symptom1 + " in " + location1 + " and " + symptom2 + " in " + location2
-     should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-'''def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
+def shoutRawText(intent, session):
     session_attributes = {}
     reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
+    
+    if 'RawText' in intent['slots']:
+        rawText = intent['slots']['RawText']['value']
+    
+    card_title = "Getting text"
+    should_end_session = True
+    speech_output = rawText
     # Setting reprompt_text to None signifies that we do not want to reprompt
     # the user. If the user does not respond or says something that is not
     # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))'''
+        card_title, speech_output, None, should_end_session))
 
 
 # --------------- Events ------------------
@@ -176,10 +114,8 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "GetSingleSymptom":
-        return findSingleSymptom(intent, session)
-    elif intent_name == "GetDualSymptom":
-        return findDualSymptom(intent, session)
+    if intent_name == "GetRawText":
+        return shoutRawText(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
